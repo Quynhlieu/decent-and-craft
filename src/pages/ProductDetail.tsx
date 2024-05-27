fb6238a12f6b51f02ac9abfb43e737f46a0d54a0
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2024-05-28 02:33:07 +0700

    create routers for our pages

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,5 +1,4 @@
 import {Box} from "@mui/material";
-import Header from "../components/Header.tsx";
 import BreadcrumbHeader from "../components/BreadcrumbHeader.tsx";
 import Detail from "../components/Detail.tsx";
 import ProductSection from "../components/ProductSection.tsx";
@@ -7,7 +6,6 @@ import BreadcrumbFooter from "../components/BreadcrumbFooter.tsx";
 const ProductDetail = () => {
     return (
     <Box sx={{paddingX:20, mb:10}}>
-        <Header/>
         <BreadcrumbHeader/>
         <Detail/>
         <ProductSection/>
