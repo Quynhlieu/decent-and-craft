6af1788eed7b5bd6fc8e976429e88b299fdb3092
Chuc-ngan <21130451@st.hcmuaf.edu.vn>
2024-05-31 22:55:18 +0700

    fix trang user

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -9,15 +9,17 @@ import MyOrder from "../components/MyOrder.tsx";
 
 const User = () => {
     return (
-        <Box sx={{ paddingX: 20 }}>
-            {/*<Header/>*/}
-            <Box sx={{ display: 'flex', width: '100%' }}>
-                <Box sx={{ flex: 2, padding: '16px' }}>
-                    <UserSpeedDial />
+        <Box>
+            <Box sx={{ paddingX: 10 }}>
+                <Box sx={{ display: 'flex', width: '100%' }}>
+                    <Box sx={{ flex: 2, padding: '10px' }}>
+                        <UserSpeedDial />
+                    </Box>
+                    <Box sx={{ flex: 10, padding: '10px' }}>
+                        <MyOrder />
+                    </Box>
                 </Box>
-                {/*<Box sx={{ flex: 10, padding: '16px' }}>*/}
-                {/*    <MyOrder />*/}
-                {/*</Box>*/}
+
             </Box>
             <ProductSection />
         </Box>
