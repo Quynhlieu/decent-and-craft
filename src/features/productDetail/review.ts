6da46ce728c5a978c1b5056ae783237558d632c4
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2024-06-24 21:59:19 +0700

    resovle review error

--- src/features/productDetail/review.ts
+++ src/features/productDetail/review.ts
@@ -2,4 +2,4 @@ import {IReview} from "../../interfaces/IProductDescription.ts";
 import {productDetails} from "../../data/productDetail.ts";
 
 
-const initialState: IReview[] = productDetails.
+// const initialState: IReview[] = productDetails
