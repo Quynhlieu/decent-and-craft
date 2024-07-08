c00728237adee708bc15a6d406538181797de8d6
Chuc-ngan <21130451@st.hcmuaf.edu.vn>
2024-07-08 20:43:03 +0700

    gan api dang ky dang nhap

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,24 +1,43 @@
-import {Box, Grid, Typography} from "@mui/material";
-import image404  from "../assets/404_error.png";
+import { Box, Grid, Typography } from "@mui/material";
+import image404 from "../assets/404_error.png";
 import React from "react";
-const page404 = () => {
+import { Link } from "react-router-dom";
+
+const Page404: React.FC = () => {
     return (
-        <Box>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-                    <img width={500} src={image404}/>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '90vh',
+                textAlign: 'center',
+                backgroundColor: '#f4f4f4',
+                padding: 3,
+            }}
+        >
+            <Grid container spacing={2} justifyContent="center" alignItems="center">
+                <Grid item xs={12} sm={6}>
+                    <img src={image404} alt="404 Error" width="80%" />
                 </Grid>
-                <Grid item xs={6}>
-                    <Typography component="h1" variant="h5">
+                <Grid item xs={12} sm={6}>
+                    <Typography component="h1" variant="h3">
                         Xin lỗi, chúng tôi không tìm thấy trang mà bạn cần!
                     </Typography>
+                    <Grid container spacing={2} columns={16}>
+                        <Grid item xs={8}>
+                            <Link to="/" className="link-home">
+                                Go Home
+                            </Link>
+                        </Grid>
+                        <Grid item xs={8}>
 
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
-
         </Box>
-
     );
 }
 
-export default page404
+export default Page404;
